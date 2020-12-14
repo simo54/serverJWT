b00e01e2d03c2c367b4950dbd47cb7c0f9f87745
simone <sorzesimone1@gmail.com>
2020-12-14 10:40:06 +0100

    implemented validation, implemented if user exist and hashing password

--- controller/user.js
+++ controller/user.js
@@ -1,13 +1,35 @@
 const User = require("../model/User");
+const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 const controller = {
+  // User Creation
   signup: async (req, res) => {
+    // Checking if there is any error on the input
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
     const { name, email, password } = req.body;
+
+    // Checking if user already exist
+    const isEmailExist = await User.findOne({ email: req.body.email });
+
+    if (isEmailExist) {
+      res.status(400).send("Email used");
+      return;
+    }
+
+    // Hashing the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPwd = await bcrypt.hash(password, salt);
+    console.log(hashedPwd);
 
     const user = new User({
       name,
       email,
-      password,
+      password: hashedPwd,
     });
     try {
       const newUser = await user.save();
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,15 @@
 const router = require("express").Router();
 const userController = require("../controller/user");
+const { body, validationResult } = require("express-validator");
 
-router.post("/register", userController.signup);
+router.post(
+  "/register",
+  [
+    body("name").isLength({ min: 3 }),
+    body("email").isEmail().isLength({ min: 3 }),
+    body("password").isLength({ min: 6 }),
+  ],
+  userController.signup
+);
 
 module.exports = router;
